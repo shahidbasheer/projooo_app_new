920a205eb3c1f5e451f7f58e4efbf74593730c04
Shahid Bashir <shahid.chodhry@gmail.com>
2016-02-18 02:25:17 +0500

    sidebar resize fix

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 jQuery(document).ready(function($) {
 
+	// file uploader
 	$('.fake-uploader').on('click', function(event) {
 		event.preventDefault();
 		 $('#editor-file').click();
@@ -12,7 +13,7 @@ jQuery(document).ready(function($) {
 	});
 
 
-
+	// contact form show hide
 	$('.form-show').on('click',  function(event) {
 
 		console.log('clicked');
@@ -27,11 +28,9 @@ jQuery(document).ready(function($) {
 		$('.contact-form').hide('fast');
 		$('.form-show').show('fast');
 		
-	
-		
 	});
 	
-
+	// form error demo
 	errortest ();
 	function errortest () {
 		$('.logign-form-wraper .error-notify , .signup-form-wraper .error-notify ').css({
@@ -40,24 +39,27 @@ jQuery(document).ready(function($) {
 				}).addClass('animated bounce');
 	}
 	
-	<!-- Menu Toggle Script -->
 	
-	$("#menu-toggle").click(function(e) {
-	    e.preventDefault();
-	    $("#wrapper").toggleClass("toggled");
-	});
 	
 		// resize dropfile div
-		resizeDropfile();
+		//wait for editor js to adjust
 		setTimeout(function(){ resizeDropfile(); }, 200);
+
 		$( window ).resize(function() {
 		 	resizeDropfile();
+		 	resizeSidebar ();
 		});
 
 		function resizeDropfile () {
 			var heigh = $(".thread-content .left").height();
 			console.log(heigh);
 				      $(".thread-editor .dropzone").css("height",heigh-28);
+		}
+		resizeSidebar ();
+		function resizeSidebar () {
+			var heigh = $(".sidebar-page .col-sm-12").height();
+			console.log(heigh);
+				      $(".sidebar-page #sidebar-wrapper").css("height",heigh);
 		}
 
 
